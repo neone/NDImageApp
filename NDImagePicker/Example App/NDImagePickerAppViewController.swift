f6eb17b85bd6befa93b80e286fc6e8d6270993e2
Dave Glassco <dave@neone.com>
2020-01-17 11:00:40 -0600

    basic image selection

--- NDImagePicker/Example App/NDImagePickerAppViewController.swift
+++ NDImagePicker/Example App/NDImagePickerAppViewController.swift
@@ -8,13 +8,37 @@
 
 import UIKit
 
-class NDImagePickerAppViewController: UIViewController {
+class NDImagePickerAppViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var frameView: UIViewX!
+    @IBOutlet weak var imageView: UIImageView! {
+        didSet {
+            if imageView != nil {
+                imageInstrucitons.isHidden = true
+            }
+        }
+    }
+    
+    @IBOutlet weak var imageInstrucitons: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func chooseImageTapped(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.sourceType = .photoLibrary
+        picker.allowsEditing = false
+        picker.delegate = self
+        present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+           guard let image = (info[.originalImage] as? UIImage) else { return }
+        
+        imageView.image = image
+        picker.dismiss(animated: true) 
+    }
 }
 
