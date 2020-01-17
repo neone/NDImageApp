7684e354f5068ce9e4cdd116b79c8dc21ccc1bb8
Dave Glassco <dave@neone.com>
2020-01-17 13:10:21 -0600

    renames

--- NDImagePickerApp/Example App/NDImagePickerAppViewController.swift
+++ NDImagePickerApp/Example App/NDImagePickerAppViewController.swift
@@ -11,18 +11,46 @@ import UIKit
 class NDImagePickerAppViewController: UIViewController, NDImagePickerDelegate {
  
     var cropperState: CropperState?
+    var isRounded = false
+    var shouldCrop = false
     
     @IBOutlet weak var frameView: UIViewX!
     @IBOutlet weak var imageView: UIImageView!
     
+    @IBOutlet weak var roundedSwitch: UISwitch!
+    @IBOutlet weak var cropSwitch: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
+    @IBAction func roundedSwitchChanged(_ sender: Any) {
+        if roundedSwitch.isOn {
+            let roundness = frameView.height / 2
+            frameView.cornerRadius = roundness
+            isRounded = true
+        } else {
+            frameView.cornerRadius = 5
+            isRounded = false
+        }
+    }
+    
     @IBAction func chooseImageTapped(_ sender: Any) {
-        let picker = NDImagePickerViewController()
+        let picker = NDImageManager()
         picker.imagePickerDelegate = self
-        picker.shouldCrop = true
+        
+        if roundedSwitch.isOn {
+            picker.isRounded = true
+        } else {
+            picker.isRounded = false
+        }
+        
+        if cropSwitch.isOn {
+            picker.shouldCrop = true
+        } else {
+            picker.shouldCrop = false
+        }
+        
         present(picker, animated: true, completion: nil)
     }
     
