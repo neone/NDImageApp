07590646b48f1490f521f09302ca23d27fa63287
Dave Glassco <dave@neone.com>
2020-01-17 07:37:56 -0600

    Initial Commit

---
+++ NDMediaPicker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NDMediaPicker
+//
+//  Created by Dave Glassco on 1/17/20.
+//  Copyright © 2020 Neone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
